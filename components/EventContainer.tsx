6b94bb7b750bc580936f7cb8386d4e58c328e91e
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2024-03-10 21:04:30 +0530

    Updated Animations

--- components/EventContainer.tsx
+++ components/EventContainer.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import events from "@/events.mjs";
+import { BackgroundGradient } from "./background-gradient";
 
 export default function EventContainer() {
     return (
@@ -13,33 +14,38 @@ export default function EventContainer() {
             <div className="flex flex-wrap justify-center">
                 {events.slice(0, 3).map((event, index) => (
                     <CardContainer key={index} className="inter-var mx-4 my-3">
-                        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-                            <CardItem
-                                translateZ="50"
-                                className="text-xl font-bold text-neutral-600 dark:text-white"
-                            >
-                                {event.name}
-                            </CardItem>
-                            <CardItem
-                                as="p"
-                                translateZ="60"
-                                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                            >
-                                {event.description}
-                            </CardItem>
-                            <CardItem translateZ="100" className="w-full mt-4">
-                                <Image
-                                    src={
-                                        event.srcImage ||
-                                        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                                    }
-                                    height="1000"
-                                    width="1000"
-                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                                    alt="thumbnail"
-                                />
-                            </CardItem>
-                        </CardBody>
+                        <BackgroundGradient className="rounded-[22px] p-1 bg-white dark:bg-zinc-900">
+                            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+                                <CardItem
+                                    translateZ="50"
+                                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                                >
+                                    {event.name}
+                                </CardItem>
+                                <CardItem
+                                    as="p"
+                                    translateZ="60"
+                                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                                >
+                                    {event.description}
+                                </CardItem>
+                                <CardItem
+                                    translateZ="100"
+                                    className="w-full mt-4"
+                                >
+                                    <Image
+                                        src={
+                                            event.srcImage ||
+                                            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+                                        }
+                                        height="1000"
+                                        width="1000"
+                                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                        alt="thumbnail"
+                                    />
+                                </CardItem>
+                            </CardBody>
+                        </BackgroundGradient>
                     </CardContainer>
                 ))}
             </div>
@@ -49,34 +55,38 @@ export default function EventContainer() {
             <div className="flex flex-wrap justify-center">
                 {events.slice(3, 6).map((event, index) => (
                     <CardContainer key={index} className="inter-var mx-4 my-3">
-                        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-                            <CardItem
-                                translateZ="50"
-                                className="text-xl font-bold text-neutral-600 dark:text-white"
-                            >
-                                {event.name}
-                            </CardItem>
-                            <CardItem
-                                as="p"
-                                translateZ="60"
-                                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                            >
-                                {event.description}
-                            </CardItem>
-                            <CardItem translateZ="100" className="w-full mt-4">
-                                <Image
-                                    src={
-                                        event.srcImage ||
-                                        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                                    }
-                                    height="1000"
-                                    width="1000"
-                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                                    alt="thumbnail"
-                                />
-                            </CardItem>
-                            
-                        </CardBody>
+                        <BackgroundGradient className="rounded-[22px] p-1  bg-white dark:bg-zinc-900">
+                            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+                                <CardItem
+                                    translateZ="50"
+                                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                                >
+                                    {event.name}
+                                </CardItem>
+                                <CardItem
+                                    as="p"
+                                    translateZ="60"
+                                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                                >
+                                    {event.description}
+                                </CardItem>
+                                <CardItem
+                                    translateZ="100"
+                                    className="w-full mt-4"
+                                >
+                                    <Image
+                                        src={
+                                            event.srcImage ||
+                                            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+                                        }
+                                        height="1000"
+                                        width="1000"
+                                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                        alt="thumbnail"
+                                    />
+                                </CardItem>
+                            </CardBody>
+                        </BackgroundGradient>
                     </CardContainer>
                 ))}
             </div>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,31 +1,40 @@
 import Link from "next/link";
-
+import { Boxes } from "./background-boxes";
+import { cn } from "@/utils/cn";
 export default function HeroSection() {
     return (
-        <section className="w-full py-12 md:py-24">
+        <section className="w-full py-12 md:py-24 relative z-20">
             <div className="container mx-auto px-4 md:px-6 items-center">
-                <div className="relative flex flex-col items-center space-y-2 text-center bg-gray-900">
-                    <div className="relative bg-opacity-70 bg-blur-lg bg-cover bg-center rounded-lg p-8">
-                        <div className="space-y-2">
-                            <h1 className="text-9xl font-extrabold tracking-tighter sm:text-10xl lg:text-14xl text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 bg-clip-text">
-                                Spark
-                            </h1>
+                <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+                    <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
-                            <p className="max-w-[600px] text-lg text-gray-200 md:text-xl/none md:mx-auto lg:text-base xl:text-xl dark:text-green-200">
-                                A Technical symposium organized by the CSE
-                                department of ADCET under the CESA.
-                            </p>
+                    <Boxes />
+                    <div className="relative flex flex-col items-center space-y-2 text-center  bg-transparent">
+                        <div className="relative bg-opacity-70 bg-blur-lg bg-cover bg-center rounded-lg p-8 z-0">
+                            <div className="space-y-2">
+                                <h1 className="text-9xl font-extrabold tracking-tighter sm:text-10xl lg:text-14xl text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 bg-clip-text z-50">
+                                    Spark
+                                </h1>
+
+                                <p className="max-w-[600px] text-lg text-gray-200 md:text-xl/none md:mx-auto lg:text-base xl:text-xl dark:text-green-200 z-50">
+                                    A Technical symposium organized by the CSE
+                                    department of ADCET under the CESA.
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex flex-wrap justify-center mt-6 ">
-                        <button className="px-8 py-3 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white font-semibold rounded-md shadow-md transition duration-300 hover:opacity-80 focus:outline-none mb-8">
-                            <Link
-                                href="https://docs.google.com/forms/d/e/1FAIpQLScrVHA5XI8Bmh2VSEQBUg2AcXfb-SmzjccU0xSNLCHaGl2-iQ/viewform"
-                                target="_"
-                            >
-                                Register Now
-                            </Link>
-                        </button>
+                        <div className="flex flex-wrap justify-center mt-6 ">
+                            <button className=" px-1 py-1 relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 z-50">
+                                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                                    <Link
+                                        href="https://docs.google.com/forms/d/e/1FAIpQLScrVHA5XI8Bmh2VSEQBUg2AcXfb-SmzjccU0xSNLCHaGl2-iQ/viewform"
+                                        target="_"
+                                    >
+                                        Register Now
+                                    </Link>
+                                </span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
